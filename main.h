83d962a497ede82b3122619dce32764d8600dba5
dennis william <dennis william>
2023-06-27 13:14:58 -0700

    adding main.h file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
